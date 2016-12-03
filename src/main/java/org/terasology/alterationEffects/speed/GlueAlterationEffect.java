c36ce9033c2cfc8690d653e388b472866e0b8aaa
TJJT777 <jared6424@icloud.com>
2016-12-03 14:48:23 -0800

    Update GlueAlterationEffect.java

--- src/main/java/org/terasology/alterationEffects/speed/GlueAlterationEffect.java
+++ src/main/java/org/terasology/alterationEffects/speed/GlueAlterationEffect.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.terasology.alterationEffects.speed;
 
 import org.terasology.alterationEffects.AlterationEffect;
@@ -6,11 +21,11 @@ import org.terasology.context.Context;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.logic.delay.DelayManager;
 
-public class WalkSpeedAlterationEffect implements AlterationEffect {
+public class GlueAlterationEffect implements AlterationEffect {
 
     private DelayManager delayManager;
 
-    public WalkSpeedAlterationEffect(Context context) {
+    public GlueAlterationEffect(Context context) {
         this.delayManager = context.get(DelayManager.class);
     }
 
@@ -20,7 +35,7 @@ public class WalkSpeedAlterationEffect implements AlterationEffect {
         GlueComponent glue = entity.getComponent(GlueComponent.class);
         if (glue == null) {
             add = true;
-            glue = new WalkSpeedComponent();
+            glue = new GlueComponent();
         }
         glue.multiplier = magnitude;
 
