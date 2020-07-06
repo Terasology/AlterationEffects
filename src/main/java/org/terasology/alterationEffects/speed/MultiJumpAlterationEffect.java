970244bab56349dfcb175e2474988f36fa2b2c97
Tobias Nett <skaldarnar@gmail.com>
2020-07-06 23:55:33 +0200

    feat: Use component-based effect for MultiJump

--- src/main/java/org/terasology/alterationEffects/speed/MultiJumpAlterationEffect.java
+++ src/main/java/org/terasology/alterationEffects/speed/MultiJumpAlterationEffect.java
@@ -15,112 +15,34 @@
  */
 package org.terasology.alterationEffects.speed;
 
-import org.terasology.alterationEffects.AlterationEffect;
 import org.terasology.alterationEffects.AlterationEffects;
+import org.terasology.alterationEffects.ComponentBasedAlterationEffect;
 import org.terasology.alterationEffects.OnEffectModifyEvent;
 import org.terasology.context.Context;
-import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.logic.delay.DelayManager;
+
+import java.util.Optional;
 
 /**
  * This handles the application of the multi jump effect, which allows an entity to jump multiple times before hitting
  * solid ground for a specified duration.
  */
-public class MultiJumpAlterationEffect implements AlterationEffect {
+public class MultiJumpAlterationEffect extends ComponentBasedAlterationEffect<MultiJumpComponent> {
 
-    private DelayManager delayManager;
-
-    /**
-     * Constructor. Instantiate an instance of this alteration effect using the provided context. This context will be
-     * used to get the DelayManager.
-     *
-     * @param context       The context which this effect will be executed on.
-     */
     public MultiJumpAlterationEffect(Context context) {
-        this.delayManager = context.get(DelayManager.class);
+        super(context, MultiJumpComponent.class, AlterationEffects.MULTI_JUMP);
     }
 
-    /**
-     * This will apply the multi jump effect on the given entity. This method will send out an event to the other
-     * applicable effect systems so that they can contribute with their own multi jump effect related modifiers.
-     *
-     * @param instigator    The entity who applied the multi jump effect.
-     * @param entity        The entity that the multi jump effect is being applied on.
-     * @param magnitude     The magnitude of the multi jump effect.
-     * @param duration      The duration of the multi jump effect.
-     */
     @Override
-    public void applyEffect(EntityRef instigator, EntityRef entity, float magnitude, long duration) {
-        // First, determine if the entity already has a multi jump component attached. If so, just replace the number
-        // of jumps multiplier, and then save the component. Otherwise, create a new one and attach it to the entity.
-        MultiJumpComponent multiJump = entity.getComponent(MultiJumpComponent.class);
-        if (multiJump == null) {
-            multiJump = new MultiJumpComponent();
-            multiJump.multiplier = magnitude;
-            entity.addComponent(multiJump);
-        } else {
-            multiJump.multiplier = magnitude;
-            entity.saveComponent(multiJump);
-        }
-
-        // Send out this event to collect all the duration and magnitude modifiers and multipliers that can affect this
-        // multi jump effect.
-        OnEffectModifyEvent effectModifyEvent = entity.send(new OnEffectModifyEvent(instigator, entity, 0, 0, this, ""));
-        long modifiedDuration = 0;
-        boolean modifiersFound = false;
-
-        // If the effect modify event is consumed, don't apply this multi jump effect.
-        if (!effectModifyEvent.isConsumed()) {
-            /*
-            Get the magnitude result value and the shortest duration, and assign them to the modifiedMagnitude and
-            modifiedDuration respectively.
-
-            The shortest duration is used as the effect modifier associated with that will expire in the shortest
-            amount of time, meaning that this effect's total magnitude and next remaining duration will have to be
-            recalculated.
-            */
-            float modifiedMagnitude = effectModifyEvent.getMagnitudeResultValue();
-            modifiedDuration = effectModifyEvent.getShortestDuration();
-
-            // If there's at least one duration and magnitude modifier, set the effect's magnitude and the modifiersFound flag.
-            if (!effectModifyEvent.getDurationModifiers().isEmpty() && !effectModifyEvent.getMagnitudeModifiers().isEmpty()) {
-                multiJump.multiplier = modifiedMagnitude;
-                modifiersFound = true;
-            }
-        }
-
-        // If the modified duration is between the accepted values (0 and Long.MAX_VALUE), and the base duration is not infinite,
-        // add a delayed action to the DelayManager using the new system.
-        if (modifiedDuration < Long.MAX_VALUE && modifiedDuration > 0 && duration != AlterationEffects.DURATION_INDEFINITE) {
-            String effectID = effectModifyEvent.getEffectIDWithShortestDuration();
-            delayManager.addDelayedAction(entity, AlterationEffects.EXPIRE_TRIGGER_PREFIX + AlterationEffects.MULTI_JUMP + "|" + effectID, modifiedDuration);
-        }
-        // Otherwise, if the duration is greater than 0, there are no modifiers found, and the effect modify event was not consumed,
-        // add a delayed action to the DelayManager using the old system.
-        else if (duration > 0 && !modifiersFound && !effectModifyEvent.isConsumed()) {
-            delayManager.addDelayedAction(entity, AlterationEffects.EXPIRE_TRIGGER_PREFIX + AlterationEffects.MULTI_JUMP, duration);
-        }
-        // Otherwise, if there are either no modifiers found, or none of the modifiers collected in the event have infinite
-        // duration, remove the component associated with this multi jump effect.
-        else if (!modifiersFound || !effectModifyEvent.getHasInfDuration()) {
-            entity.removeComponent(MultiJumpComponent.class);
-        }
-        // If this point is reached and none of the above if-clauses were met, that means there was at least one modifier
-        // collected in the event which has infinite duration.
+    protected MultiJumpComponent upsertComponent(Optional<MultiJumpComponent> maybeComponent, float magnitude,
+                                                 long duration) {
+        MultiJumpComponent multiJump = maybeComponent.orElse(new MultiJumpComponent());
+        multiJump.multiplier = magnitude;
+        return multiJump;
     }
 
-    /**
-     * This will apply the multi jump effect on the given entity by calling the method
-     * {@link #applyEffect(EntityRef, EntityRef, float, long)}.
-     *
-     * @param instigator    The entity who applied the multi jump effect.
-     * @param entity        The entity that the multi jump effect is being applied on.
-     * @param id            Inapplicable to the multi jump effect.
-     * @param magnitude     The magnitude of the multi jump effect.
-     * @param duration      The duration of the multi jump effect.
-     */
     @Override
-    public void applyEffect(EntityRef instigator, EntityRef entity, String id, float magnitude, long duration) {
-        applyEffect(instigator, entity, magnitude, duration);
+    protected MultiJumpComponent updateComponent(OnEffectModifyEvent event, MultiJumpComponent component) {
+        component.multiplier = event.getMagnitudeResultValue();
+        return component;
     }
 }
