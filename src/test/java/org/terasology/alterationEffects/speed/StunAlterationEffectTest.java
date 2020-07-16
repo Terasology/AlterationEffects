54bbb6e4cf0d2b2e4180c701ed7955b07e714357
Josephine Rueckert <jd.rueckert@googlemail.com>
2020-07-16 23:55:08 +0200

    fix: unsuccessfully try to get `DelayManager` to work

--- src/test/java/org/terasology/alterationEffects/speed/StunAlterationEffectTest.java
+++ src/test/java/org/terasology/alterationEffects/speed/StunAlterationEffectTest.java
@@ -7,13 +7,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.terasology.alterationEffects.AlterationEffect;
+import org.terasology.alterationEffects.AlterationEffects;
 import org.terasology.alterationEffects.EffectsAuthoritySystem;
 import org.terasology.context.Context;
 import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.entity.lifecycleEvents.OnActivatedComponent;
+import org.terasology.entitySystem.entity.lifecycleEvents.OnAddedComponent;
 import org.terasology.logic.delay.DelayManager;
 import org.terasology.logic.delay.DelayedActionComponent;
 import org.terasology.logic.delay.DelayedActionTriggeredEvent;
 import org.terasology.logic.players.LocalPlayer;
+import org.terasology.math.geom.Vector3i;
 import org.terasology.moduletestingenvironment.MTEExtension;
 import org.terasology.moduletestingenvironment.ModuleTestingHelper;
 import org.terasology.moduletestingenvironment.TestEventReceiver;
@@ -37,20 +41,23 @@ class StunAlterationEffectTest {
 
     @Test
     void applyEffect() {
+        helper.forceAndWaitForGeneration(new Vector3i());
         Context context = helper.createClient();
-        context.put(DelayManager.class, delayManager);
+        //context.put(DelayManager.class, delayManager);
+        TestEventReceiver receiveOnActivatedComponent = new TestEventReceiver<>(context, OnActivatedComponent.class, (event, entity) -> {}, DelayedActionComponent.class);
+        TestEventReceiver receiveDelayedActionTriggered = new TestEventReceiver<>(context, DelayedActionTriggeredEvent.class, (event, entity) -> {
+            String actionId = event.getActionId();
+            Assertions.assertEquals("AlterationEffects:Expire:STUN", actionId);
+        });
+
         EntityRef player = context.get(LocalPlayer.class).getCharacterEntity();
-        StunAlterationEffect stunEffect = new StunAlterationEffect(context);
+        AlterationEffect stunEffect = effectsAuthoritySystem.getEffect(AlterationEffects.STUN);
         stunEffect.applyEffect(player, player, 5, 1000);
 
         Assertions.assertTrue(player.hasComponent(StunComponent.class), "Stun component not found");
         Assertions.assertTrue(player.hasComponent(DelayedActionComponent.class), "Delayed action component not found");
-        TestEventReceiver receiver = new TestEventReceiver<>(context, DelayedActionTriggeredEvent.class, (event, entity) -> {
-           String actionId = event.getActionId();
-           Assertions.assertEquals("AlterationEffects:Expire:STUN", actionId);
-        });
-
-        Assertions.assertFalse(receiver.getEvents().isEmpty(), "No events received");
+        Assertions.assertFalse(receiveOnActivatedComponent.getEvents().isEmpty(), "Delayed action component not activated");
+        Assertions.assertFalse(receiveDelayedActionTriggered.getEvents().isEmpty(), "No events received");
 
         boolean timedOut = helper.runWhile(5000, ()-> true);
         Assertions.assertTrue(timedOut, "Didn't time out");
