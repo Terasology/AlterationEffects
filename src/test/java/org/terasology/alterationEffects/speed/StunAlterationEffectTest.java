b08d89ba8441da4fbcfff24852b4b8faab6d9ca1
Tobias Nett <skaldarnar@googlemail.com>
2020-07-17 20:19:50 +0200

    wait until all engines are in-game

--- src/test/java/org/terasology/alterationEffects/speed/StunAlterationEffectTest.java
+++ src/test/java/org/terasology/alterationEffects/speed/StunAlterationEffectTest.java
@@ -10,10 +10,9 @@ import org.terasology.alterationEffects.AlterationEffect;
 import org.terasology.alterationEffects.AlterationEffects;
 import org.terasology.alterationEffects.EffectsAuthoritySystem;
 import org.terasology.context.Context;
+import org.terasology.engine.modes.StateIngame;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.entity.lifecycleEvents.OnActivatedComponent;
-import org.terasology.entitySystem.entity.lifecycleEvents.OnAddedComponent;
-import org.terasology.logic.delay.DelayManager;
 import org.terasology.logic.delay.DelayedActionComponent;
 import org.terasology.logic.delay.DelayedActionTriggeredEvent;
 import org.terasology.logic.players.LocalPlayer;
@@ -24,8 +23,6 @@ import org.terasology.moduletestingenvironment.TestEventReceiver;
 import org.terasology.moduletestingenvironment.extension.Dependencies;
 import org.terasology.registry.In;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @ExtendWith(MTEExtension.class)
 @Dependencies({"engine", "AlterationEffects"})
 class StunAlterationEffectTest {
@@ -34,21 +31,25 @@ class StunAlterationEffectTest {
     private ModuleTestingHelper helper;
 
     @In
-    private DelayManager delayManager;
-
-    @In
     private EffectsAuthoritySystem effectsAuthoritySystem;
 
     @Test
     void applyEffect() {
         helper.forceAndWaitForGeneration(new Vector3i());
         Context context = helper.createClient();
-        //context.put(DelayManager.class, delayManager);
-        TestEventReceiver receiveOnActivatedComponent = new TestEventReceiver<>(context, OnActivatedComponent.class, (event, entity) -> {}, DelayedActionComponent.class);
-        TestEventReceiver receiveDelayedActionTriggered = new TestEventReceiver<>(context, DelayedActionTriggeredEvent.class, (event, entity) -> {
-            String actionId = event.getActionId();
-            Assertions.assertEquals("AlterationEffects:Expire:STUN", actionId);
-        });
+        TestEventReceiver<OnActivatedComponent> receiveOnActivatedComponent = new TestEventReceiver<>(context,
+                OnActivatedComponent.class, (event, entity) -> {
+        }, DelayedActionComponent.class);
+        TestEventReceiver<DelayedActionTriggeredEvent> receiveDelayedActionTriggered =
+                new TestEventReceiver<>(context, DelayedActionTriggeredEvent.class, (event, entity) -> {
+                    String actionId = event.getActionId();
+                    Assertions.assertEquals("AlterationEffects:Expire:STUN", actionId);
+                });
+
+        Assertions.assertFalse(
+                helper.runUntil(1000,
+                        () -> helper.getEngines().stream().allMatch(engine -> engine.getState().getClass().equals(StateIngame.class))),
+                "Engine(s) not in state 'Ingame'!");
 
         EntityRef player = context.get(LocalPlayer.class).getCharacterEntity();
         AlterationEffect stunEffect = effectsAuthoritySystem.getEffect(AlterationEffects.STUN);
@@ -56,10 +57,11 @@ class StunAlterationEffectTest {
 
         Assertions.assertTrue(player.hasComponent(StunComponent.class), "Stun component not found");
         Assertions.assertTrue(player.hasComponent(DelayedActionComponent.class), "Delayed action component not found");
-        Assertions.assertFalse(receiveOnActivatedComponent.getEvents().isEmpty(), "Delayed action component not activated");
+        Assertions.assertFalse(receiveOnActivatedComponent.getEvents().isEmpty(), "Delayed action component not " +
+                "activated");
         Assertions.assertFalse(receiveDelayedActionTriggered.getEvents().isEmpty(), "No events received");
 
-        boolean timedOut = helper.runWhile(5000, ()-> true);
+        boolean timedOut = helper.runWhile(5000, () -> true);
         Assertions.assertTrue(timedOut, "Didn't time out");
         Assertions.assertFalse(player.hasComponent(StunComponent.class), "Component not removed");
     }
