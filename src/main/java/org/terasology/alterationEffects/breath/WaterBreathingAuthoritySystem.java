e96f30f4a49f55bab2f4a356b662e92ed6c42e59
Tobias Nett <skaldarnar@googlemail.com>
2021-02-06 14:15:29 +0100

    feat(JOML): migrate `BlockComponent`
    Depends on MovingBlocks/Terasology#4470

--- src/main/java/org/terasology/alterationEffects/breath/WaterBreathingAuthoritySystem.java
+++ src/main/java/org/terasology/alterationEffects/breath/WaterBreathingAuthoritySystem.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.alterationEffects.breath;
 
 import org.terasology.entitySystem.entity.EntityRef;
@@ -39,7 +26,7 @@ public class WaterBreathingAuthoritySystem extends BaseComponentSystem {
     @ReceiveEvent
     public void cancelDamageFromWater(BeforeDamagedEvent event, EntityRef entity, WaterBreathingComponent component) {
         final BlockComponent block = event.getInstigator().getComponent(BlockComponent.class);
-        if (block != null && block.getBlock().getURI().equals(new BlockUri("CoreAssets:Water"))) {
+        if (block != null && block.block.getURI().equals(new BlockUri("CoreAssets:Water"))) {
             event.consume();
         }
     }
