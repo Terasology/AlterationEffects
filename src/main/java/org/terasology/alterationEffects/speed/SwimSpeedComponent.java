fbc5f2f7e033e388841534ae0566d03c3fd121ab
Michael Pollind <mpollind@gmail.com>
2021-12-05 11:26:51 -0800

    fix checkstyle

--- src/main/java/org/terasology/alterationEffects/speed/SwimSpeedComponent.java
+++ src/main/java/org/terasology/alterationEffects/speed/SwimSpeedComponent.java
@@ -1,5 +1,5 @@
 // Copyright 2021 The Terasology Foundation
-// SPDX-License-Identifier: Apache-2.0// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.alterationEffects.speed;
 
 import org.terasology.gestalt.entitysystem.component.Component;
